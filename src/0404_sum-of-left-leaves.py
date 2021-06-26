6b04026060431cdc68e44858762b83d939c2ae57
Ismayil Ismayilov <iismayilov4574@ada.edu.az>
2021-06-26 23:03:41 +0400

    Update Sum of Left Leaves

--- src/0404_sum-of-left-leaves.py
+++ src/0404_sum-of-left-leaves.py
@@ -1,17 +1,17 @@
 class Solution:
-    self.left_leaf_sum = 0
+    left_leaf_sum = 0
 
     def is_leaf(self, node):
         return not node.left and not node.right
 
-    def traverse(self, node, is_left=True):
+    def traverse(self, node, is_left=False):
         if not node:
             return None
 
         if is_left and self.is_leaf(node):
             self.left_leaf_sum += node.val
 
-        self.traverse(node.left)
+        self.traverse(node.left, is_left=True)
         self.traverse(node.right, is_left=False)
 
     def sumOfLeftLeaves(self, root: TreeNode) -> int:
